0c6b8ded3292cf841507233680054f285090d903
PauloAlves8039 <pj38alves@gmail.com>
2020-09-02 00:23:49 -0300

    Ajustando arquivos

--- GetProducts/index.js
+++ GetProducts/index.js
@@ -2,7 +2,7 @@ const createMongoClient = require('../shared/mongoClient');
 const { MongoClient } = require('mongodb');
 
 module.exports = async function (context, req) {
-    
+
     const { client: MongoClient, closeConnectionFn } = await createMongoClient();
     const Products = MongoClient.collection('products');
     const res = await Products.find({});
